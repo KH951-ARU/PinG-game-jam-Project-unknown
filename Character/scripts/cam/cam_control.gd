25dc00dcfda38cd730c03ef6c4bceea07df0abd9
KH951-ARU <KH951@student.aru.ac.uk>
2026-06-23 00:06:00 +0100

    week 2 begin
    reserached and fix previous bugs and started on the player movement.

---
+++ Character/scripts/cam/cam_control.gd
@@ -0,0 +1,36 @@
+extends Node
+## variables for camera system
+
+@onready var h_node =$"h(yaw)"
+@onready var w_node =$"h(yaw)/w(pitch)"
+@onready var cam = $"h(yaw)/w(pitch)/SpringArm3D/Camera3D"
+
+var hyaw : float = 0
+var wpitch : float = 0
+var h_sensitivity : float = 0.07
+var w_sensitivity : float = 0.07
+var h_acceleration : float = 15
+var w_acceleration : float = 15
+
+var pitch_max : float = 75 
+var pitch_min : float = -55
+
+## functions and operations
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		hyaw += event.relative.x * h_sensitivity
+		wpitch += -event.relative.y * w_sensitivity
+		
+
+func _physics_process(delta):
+	wpitch = clamp( wpitch , pitch_min , pitch_max)
+	
+	h_node.rotation_degrees.y = lerp(h_node.rotation_degrees.y, hyaw, h_acceleration * delta)
+	w_node.rotation_degrees.x = lerp(w_node.rotation_degrees.x, wpitch, w_acceleration * delta)
+	
+	#h_node.rotation_degrees.y = hyaw
+	#w_node.rotation_degrees.x = wpitch
+ 
---
+++ Character/scripts/msm/movement_state.gd
@@ -0,0 +1,10 @@
+extends Resource
+class_name Movementstate
+
+##Varibles for states
+
+@export var id : int
+@export var movementspeed : float
+@export var acceleration : float = 6
+@export var camera_fov : float = 75
+@export var animationspeed : float = 1
