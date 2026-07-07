2fcda0ada83eab703b74d08d376f530238adfa10
Kieran Harwood <KH951@student.aru.ac.uk>
2026-07-07 13:18:39 +0100

    stances 1.1.0

--- Character/stance.gd
+++ Character/stance.gd
@@ -10,7 +10,7 @@ class_name Stance
 @export_category("camera data")
 @export var camera_height : float = 1.3
 
-@export_category("collisioon data")
+@export_category("collison data")
 @export var col_raycast : RayCast3D
 @export var collider : CollisionObject3D
 @export var higher_stances :Array[Stance]
